14c781a18123deea0966e18edb350555353a151a
Paul Pineda <paul@pineda.codes>
2017-05-29 17:42:41 +0100

    Extracted relevant keys array into it's own file.  Defined getcJSONToArray test.

---
+++ app/dataAPI/relevantContentKeys.js
@@ -0,0 +1,2 @@
+// relevant keys to extract from GETC query serialsied JSON objects .COLUMNS property
+module.exports = ['scoid','sequenceno','type','prominence','content','autoid','status','css_width','css_valign','css_hide','css_halign','css_marginleft','css_marginright','css_margintop','css_marginbottom','css_maxwidth','css_delay','css_entry'];
--- test/dataAPI/getcJSONToArray.test.js
+++ test/dataAPI/getcJSONToArray.test.js
@@ -1,15 +1,24 @@
-const assert = require('assert');
-const dummyJSON = require('../../app/dataAPI/_dummy_0JSONSerialisedContent');
-describe('Create flatArray from content JSON', function(){
-  describe('filter COLUMNS to only useful ones', function(){
-    it('has a function')
+const assert = require('chai').assert,
+  dummyJSON = require('../../app/dataAPI/_dummy_0JSONSerialisedContent'),
+  getc2Array = require('../../app/dataAPI/getcJSONToArray');
+  relevantKeys = require('../../app/dataAPI/relevantContentKeys');
+
+describe('API for handling serialised JSON from Skillcast CFML content query', function(){
+  // array of columns we are interested in
+  //const relevantKeysIndex = [0,2,3,4,5,10,11,17,18,19,23,24,25,26,27,28,29,30];
+  const filterCOLUMNS = getc2Array.filterCOLUMNS;
+  let targetCOLUMNS = relevantKeys.concat(['parentid']);
+
+  it('has a method \'.filterCOLUMNS\' that creates an array of indexes for the relevant keys from COLUMNS', function(){
+    assert.isFunction(filterCOLUMNS, 'filterMethod is defined and is a function');
+    assert.deepEqual(filterCOLUMNS(dummyJSON.COLUMNS), relevantKeys);
+  });
+
+  it('has a method \'.toArrayOfObjectsWithParentId\' that reduces the DATA array of arrays into array of objects each with keys that match targetCOLUMNS', function(){
+    var reducedContent = getc2Array
+      .toArrayOfObjectsWithParentId(dummyJSON)
+      .forEach(function(c,i,a){
+        assert.hasAllKeys(targetCOLUMNS);
+      });
   });
 });
-
-
-
-// reduces the COLUMN and DATA arrays into an array of content elements
-// - each element will have a 'parent' attribute added to it according to the
-//   chronological order it appears in the array
-//
-// RETURNS
